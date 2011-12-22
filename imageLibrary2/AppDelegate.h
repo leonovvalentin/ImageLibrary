793afba40853bd706f1e54da016471bc165eb9be
admin <admin@h60n143.campus.nsu.ru>
2011-12-22 15:37:36 +0600

    Initial Commit

---
+++ imageLibrary2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  imageLibrary2
+//
+//  Created by admin on 22.12.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
